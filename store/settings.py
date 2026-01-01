e7b74d397fbb3201d0cb9460a6af8346e7223f85
Manavbarodiya <2002manavbarodiya@gmail.com>
2026-01-01 12:40:59 +0530

    Make settings use environment variables for production

--- store/settings.py
+++ store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b=_x@$8cf2jv6&mp(=fb@y#yja_$1)u#a6h^q%=#zxpeh98m24'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-b=_x@$8cf2jv6&mp(=fb@y#yja_$1)u#a6h^q%=#zxpeh98m24')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['e-commerce-store-2bqi.onrender.com', 'localhost', '127.0.0.1']
+if os.environ.get('ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
